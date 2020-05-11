6413da16f464daae7b70087dfd324c861a700870
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-11 20:42:49 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial.split(" ").map(w => w[0].toUpperCase() + w.slice(1)).join(" ")
+  });
 }
